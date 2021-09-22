15edaf0466baedc7f9cdc52ebd8997b33f7e4196
Shuvam Shah <shvmks@outlook.com>
2021-09-22 13:52:08 +0530

    Add endpoints
    * Predict info types
    
    * Generate summary
    
    * Get/post/detail sub-summary
    
    Post main summary

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Depends
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+from typing import List
 
 from api.database import SessionLocal
 from api import schemas, crud
@@ -24,6 +25,54 @@ def api_home():
     'desc': 'API for accessing Issue-Analysis models.'
   }
 
-@app.get("/api/main-summary/{gh_user}/{repo}/", response_model=schemas.MainSummary)
+@app.get("/api/{gh_user}/{repo}/main-summary/", response_model=schemas.MainSummary)
 def get_main_summary(gh_user: str, repo: str, db: Session = Depends(get_db_session)):
+  """
+  Get main summary for the given repository.
+  """
   return crud.get_main_summary(gh_user, repo, db)
+
+@app.post("/api/{gh_user}/{repo}/main-summary/")
+def post_main_summary(gh_user: str, repo: str, summary: schemas.MainSummary, db: Session = Depends(get_db_session)):
+  """
+  Post main summary for the given repository.
+  Workflow: Generate main summary -> user edits -> post
+  """
+  pass
+
+@app.post("/api/generate-summary/", response_model=schemas.SummaryText)
+def generate_summary(text: str):
+  """
+  Generates summary from text.
+  """
+  pass
+
+@app.post("/api/information-type/", response_model=List[schemas.Sentence])
+def predict_information_type(comment: str):
+  """
+  Predict information types in sentences of a comment. Performs sentence splitting as well.
+  """
+  pass
+
+@app.get("/api/{gh_user}/{repo}/sub-summary/", response_model=List[schemas.SubSummary])
+def get_sub_summaries(gh_user: str, repo: str, db: Session = Depends(get_db_session)):
+  """
+  Gets a list of sub-summaries for a particular repository.
+  Query each sub-summary for details.
+  """
+  pass
+
+@app.get("/api/{gh_user}/{repo}/sub-summary/{sub_summary_id}", response_model=schemas.SubSummaryDetail)
+def get_sub_summary_details(gh_user: str, repo: str, sub_summary_id: int, db: Session = Depends(get_db_session)):
+  """
+  Gets details of a particular sub summary.
+  """
+  pass
+
+@app.post("/api/{gh_user}/{repo}/sub-summary/", response_model=schemas.SubSummaryDetail)
+def post_sub_summary(gh_user: str, repo: str, sub_summary: schemas.SubSummaryDetail,
+                     db: Session = Depends(get_db_session)):
+  """
+  Posts sub summary for the given repository.
+  """
+  pass
--- api/schemas.py
+++ api/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from typing import List
+from datetime import datetime
+from typing import List, Optional
 
 class InfoTypeSummary(BaseModel):
   type_id: int
@@ -7,3 +8,30 @@ class InfoTypeSummary(BaseModel):
 
 class MainSummary(BaseModel):
   summaries: List[InfoTypeSummary]
+
+class SummaryText(BaseModel):
+  summary: str
+
+class Sentence(BaseModel):
+  sentence: str
+  offset: int
+  limit: int
+  type_id: Optional[int]
+
+class Author(BaseModel):
+  name: str
+  link: str
+
+class SubSummary(BaseModel):
+  id: Optional[int]
+  summary: str
+  n_comments: int
+  author: Author
+
+class Comment(BaseModel):
+  text: str
+  author: Author
+  commented_on: datetime
+
+class SubSummaryDetail(SubSummary):
+  comments: List[Comment]
