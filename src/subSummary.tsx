bfcd92a326d529ea4481a7bde751986082028f6c
Avinash Bhat <avinash.bhat@mail.mcgill.ca>
2021-10-31 19:57:35 -0400

    Added the edit icon and the listener

--- src/subSummary.tsx
+++ src/subSummary.tsx
@@ -60,19 +60,31 @@ class Summary {
 }
 
 class SummaryComponent extends React.Component<
-  { summaries: any, editExistingSummary },
+  { summaries: any, viewExistingSummary, viewing: string },
   {}
 > {
   render() {
     let summaryContent: Array<Element> = [];
-    this.props.summaries.forEach((s) =>
-      summaryContent.push(
-        <div className="Box flex-column m-1 p-1 color-border-info"
-          onClick={() => { this.props.editExistingSummary(s.id); }}>
-          {s.summary}
-        </div>
-      )
-    );
+    this.props.summaries.forEach((s) => {
+      if (this.props.viewing && this.props.viewing === s.id) {
+        summaryContent.push(
+          <div className="Box flex-column m-1 p- color-border-info"
+            onClick={() => { this.props.viewExistingSummary(s.id); }}>
+            <button className="btn-octicon float-right" type="button" aria-label="Pencil icon">
+              <svg className="octicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M11.013 1.427a1.75 1.75 0 012.474 0l1.086 1.086a1.75 1.75 0 010 2.474l-8.61 8.61c-.21.21-.47.364-.756.445l-3.251.93a.75.75 0 01-.927-.928l.929-3.25a1.75 1.75 0 01.445-.758l8.61-8.61zm1.414 1.06a.25.25 0 00-.354 0L10.811 3.75l1.439 1.44 1.263-1.263a.25.25 0 000-.354l-1.086-1.086zM11.189 6.25L9.75 4.81l-6.286 6.287a.25.25 0 00-.064.108l-.558 1.953 1.953-.558a.249.249 0 00.108-.064l6.286-6.286z"></path></svg>
+            </button>
+            <div className="m-1">{s.summary}</div>
+          </div>
+        )
+      } else {
+        summaryContent.push(
+          <div className="Box flex-column m-1 p-1 color-border-info"
+            onClick={() => { this.props.viewExistingSummary(s.id); }}>
+            {s.summary}
+          </div>
+        )
+      }
+    });
     return summaryContent;
   }
 }
@@ -304,7 +316,7 @@ class CommentComponent extends React.Component<
 
 class SubSummaryComponent extends React.Component<
   {},
-  { subsummaries: Array<Summary>; editing: string; visible: string; }
+  { subsummaries: Array<Summary>; editing: string; visible: string; viewing: string }
 > {
   addedComments: Array<string>;
   constructor(props) {
@@ -314,6 +326,7 @@ class SubSummaryComponent extends React.Component<
       subsummaries: [],
       editing: "",
       visible: "summary",
+      viewing: ""
     };
     this.loadCommentComponents = this.loadCommentComponents.bind(this);
     this.saveSummary == this.saveSummary.bind(this);
@@ -393,9 +406,9 @@ class SubSummaryComponent extends React.Component<
     return new IssueComment(c_id, c_body, c_bodytext, author);
   };
 
-  editExistingSummary = (id: string) => {
+  viewExistingSummary = (id: string) => {
     this.setState({
-      editing: id
+      viewing: id
     });
   }
 
@@ -423,10 +436,9 @@ class SubSummaryComponent extends React.Component<
         if (s.summary) {
           allSummaries.push({ summary: s.summary, id: s.id });
         }
-
       });
       if (allSummaries.length) {
-        return <SummaryComponent summaries={allSummaries} editExistingSummary={this.editExistingSummary} />;
+        return <SummaryComponent summaries={allSummaries} viewExistingSummary={this.viewExistingSummary} viewing={this.state.viewing} />;
       }
     }
     return (
@@ -530,7 +542,7 @@ class SubSummaryComponent extends React.Component<
 
   render() {
     let navbarContent;
-    if (this.state.editing && (this.state.visible === "comments")) {
+    if ((this.state.editing || this.state.viewing) && (this.state.visible === "comments")) {
       this.state.subsummaries.forEach(ss => {
         if (ss.id === this.state.editing) {
           navbarContent = ss.comments;
