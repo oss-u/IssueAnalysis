00f25088efa799bdc896f82ac98a12179af4b526
ChevinCherry <kevin.cherry@mail.mcgill.ca>
2022-02-03 15:54:46 -0500

    Added edit mode to information summaries, reformatted top level summary to a functional component

---
+++ src/components/InformationType.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import "../style.scss";
+
+interface InformationTypeProps {
+    title: string;
+    content: string;
+    tooltip: string;
+    editing: boolean;
+    onClick: () => void;
+}
+
+export default function InformationType(props: InformationTypeProps): JSX.Element {
+    const {title, content, tooltip, editing, onClick} = props
+
+    const renderContent = (content: string, editing: boolean) => {
+        if (editing) {
+            return <textarea className="form-control width-full">{content}</textarea>
+        }
+        return <p>{content}</p>
+    }
+
+    const onNonEditClick = () => {
+        if (!editing){
+            onClick()
+        }
+    }
+
+    return (
+        <div className="Box-body" onClick={onNonEditClick}>
+          <div className="tooltipped tooltipped-se" aria-label={tooltip}>
+            <h4>{title}</h4>
+          </div>
+          {renderContent(content, editing)}
+        </div>
+      );
+}
