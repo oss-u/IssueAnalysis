a871cb35c0492a58d2bb4dfb32aeb05e9f34a3da
ChevinCherry <kevin.cherry@mail.mcgill.ca>
2022-02-11 12:08:19 -0500

    Information type sentence highlighting and navigation

---
+++ src/utils/index.ts
@@ -0,0 +1,24 @@
+export const getCurrentUserName = () => {
+    const userNameEl = document.querySelector('head > meta[name="user-login"]');
+    return userNameEl.getAttribute("content")
+}
+
+export function guidGenerator() {
+    var S4 = function () {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (
+      S4() +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      S4() +
+      S4()
+    );
+}
