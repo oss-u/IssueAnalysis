a91030f412329390ccad9f8334d5d1efe3c9a0f3
Avinash Bhat <avinash.bhat@mail.mcgill.ca>
2022-03-22 17:31:53 -0400

    Top level summary abstracted

--- src/components/InformationTypeSummary.tsx
+++ src/components/InformationTypeSummary.tsx
@@ -1,129 +1,22 @@
 import ReactDOM from "react-dom";
-import React, { useEffect } from "react";
+import React from "react";
 import "../style.scss";
-import { informationTypeMap } from "../utils/maps";
-import { TabNav, Box } from "@primer/components";
-import { default as InformationTypeComponent } from "./InformationType";
-import { getCurrentUserName } from "../utils";
-import TopLevelNavBox from "./TopLevelNavBox";
-import { InformationType } from "../types";
+import { InformationTypeTabs } from "./InformationType";
 
-interface SummaryType {
-  typeId: number;
-  content: string;
-}
-
-const mapInfoIdToType: { [id: number]: InformationType } = {
-  1: "expectedBehaviour",
-  2: "motivation",
-  6: "solutionDiscussion",
-}
 
 export default function InformationTypeSummary(): JSX.Element {
-  const [informationTypeSummaries, setInformationTypeSummaries] =
-    React.useState<SummaryType[]>([]);
-  // Stores the temporary edited state of the summaries before save
-  const [editedSummaries, setEditedSummaries] = React.useState<SummaryType[]>(
-    informationTypeSummaries
-  );
   const [visible, setVisible] = React.useState<boolean>(false);
-  const [editing, setEditing] = React.useState<boolean>(false);
-  const [authors, setAuthors] = React.useState<string[]>([]);
-  const [initNavInfoType, setInitNavInfoType] = React.useState<InformationType>("none");
+  const [infoTypes, setInfotypes] = React.useState<JSX.Element>(null);
 
-  const [infoTypes, setInfotypes] = React.useState([]);
-  const [infoTypeContents, setInfoTypeContents] = React.useState(null);
-
-  useEffect(() => {
-    setEditedSummaries(informationTypeSummaries);
-  }, [informationTypeSummaries]);
 
   const initializeTopLevelSummary = () => {
-    // API specifications - to get the summary
-    // Should return an array. Array contains
-    // tuples with summary and the information type ID.
-    let summaries = [
-      {
-        typeId: 1,
-        content:
-          "I would like to propose an additional instance method to the ensemble estimators to fit additional sub estimators.",
-      },
-      { typeId: 2, content: "There is something similar in adaboost!" },
-      {
-        typeId: 6,
-        content:
-          "I don't know if fit_extends is the best solution to the problem.",
-      },
-    ];
-    // generateInfoTypeContents(summaries);
-    generateTabs(summaries);
-    setInformationTypeSummaries(summaries);
+    setInfotypes(<InformationTypeTabs />);
     setVisible(true);
   };
 
-  const generateInfoTypeContents = (summaries, hrf) => {
-
-    summaries.map((infoType) => {
-      if (informationTypeMap.get(infoType.typeId).tabLink === hrf) {
-        const newElement = (<InformationTypeComponent
-          title={informationTypeMap.get(infoType.typeId).title}
-          tooltip={informationTypeMap.get(infoType.typeId).tooltip}
-          content={infoType.content}
-          editing={editing}
-          tabLink={informationTypeMap.get(infoType.typeId).tabLink}
-          onClick={() => { setEditing(true) }}
-          onEdit={onEditSummary(infoType.typeId)}
-        />)
-        setInfoTypeContents(newElement);
-      }
-      // setInfoTypeContents(oldArray => [...oldArray, newElement]);
-    });
-  }
-
-  const generateTabs = (summaries) => {
-    summaries.map((infoType) => {
-      const newElement = (<TabNav.Link href="#topLevelSummary" onClick={() => {
-        // add smooth scroll to the box div
-        generateInfoTypeContents(summaries, informationTypeMap.get(infoType.typeId).tabLink);
-      }}>
-        {informationTypeMap.get(infoType.typeId).title}
-      </TabNav.Link>)
-      setInfotypes(oldArray => [...oldArray, newElement]);
-    })
-  }
-
-  const onEditSummary = (idNum: number) => {
-    return (content: string) => {
-      const newSummaries = [...editedSummaries];
-      const editedSummary = {
-        typeId: idNum,
-        content,
-      };
-      const editedIndex = newSummaries.findIndex(
-        (sumType) => sumType.typeId === idNum
-      );
-      newSummaries[editedIndex] = editedSummary;
-      setEditedSummaries(newSummaries);
-    };
-  };
-
-  const onSave = () => {
-    setInformationTypeSummaries(editedSummaries);
-    const newAuthors = new Set(authors)
-    newAuthors.add(getCurrentUserName())
-    setAuthors(Array.from(newAuthors.values()));
-    setEditing(false);
-  };
-
-  const onClickInfoType = (infoTypeId: number) => {
-    const infoType = mapInfoIdToType[infoTypeId];
-    setInitNavInfoType(infoType);
-  }
-
-
   return (
     <div>
-      {initNavInfoType !== "none" && <TopLevelNavBox initInfoType={initNavInfoType} onClose={() => setInitNavInfoType("none")} />}
+      {/* {initNavInfoType !== "none" && <TopLevelNavBox initInfoType={initNavInfoType} onClose={() => setInitNavInfoType("none")} />} */}
       <div id="topLevelSummary" className="Box">
         <div className="Box-header">
           <div className="clearfix">
@@ -137,8 +30,11 @@ export default function InformationTypeSummary(): JSX.Element {
                     id="minimiseButton"
                     className="btn btn-sm"
                     type="button"
-                    aria-disabled={informationTypeSummaries.length === 0 ? "true" : "false"}
-                    onClick={() => setVisible(!visible)}
+                    aria-disabled={(infoTypes != null) ? "false" : "true"}
+                    onClick={() => {
+                      if (infoTypes != null)
+                        setVisible(!visible);
+                    }}
                   >
                     {visible ? "Hide" : "Show"}
                   </button>
@@ -157,37 +53,9 @@ export default function InformationTypeSummary(): JSX.Element {
       </div>
       {visible && (
         <div id="infoTypesSummaryDiv">
-          <TabNav aria-label="information-types" id="infotype-summary">
-            {infoTypes}
-          </TabNav>
-          <Box>
-            {infoTypeContents}
-          </Box>
+          {infoTypes}
         </div>
       )}
-      {visible && (
-        <div id="editButtons" className="d-flex flex-justify-end mt-2">
-          {editing && (
-            <>
-              <div className="btn mr-2" onClick={() => setEditing(false)}>
-                Back
-              </div>
-              <div className="btn btn-primary" onClick={onSave}>
-                Save
-              </div>
-            </>)}
-          {
-            !editing && (
-              <>
-                {authors.length > 0 && (
-                  <div className="lh-condensed text-semibold f6 mr-3">
-                    Summary by {authors.join(", ")}
-                  </div>)}
-                <div className="btn" onClick={() => setEditing(true)}>
-                  Edit
-                </div>
-              </>)}
-        </div>)}
     </div>
   );
 }
